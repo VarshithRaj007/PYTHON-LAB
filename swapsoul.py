998124320f10439ebe735fd409a1a5421bc6370a
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-07-16 12:18:58 +0530

    Add files via upload

---
+++ swapsoul.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number\n"))
+b = int(input("Enter the second number\n"))
+print("Numbers before swapping are\n",a,b)
+a = a + b
+b = a - b
+a = a - b
+print("Numbers after swapping are\n",a,b)
