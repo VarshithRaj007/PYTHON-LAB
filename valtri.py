0bdfe148dc93d2d65568165cc7563a3af0fd3340
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-07-16 12:20:35 +0530

    Add files via upload

---
+++ valtri.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the length of first side"))
+b = int(input("Enter the length of second side"))
+c = int(input("Enter the length of third side"))
+if a+b>c or a+c>b or b+c>a :
+    print("The triangle is Valid")
+else :
+    print("The triangle is invalid")
+
