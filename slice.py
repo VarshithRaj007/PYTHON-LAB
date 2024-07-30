a88552869c8c53e31fbcb4ae16f540e4d84ef8d2
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-07-30 11:47:19 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,14 @@
+l = ['a','b','c','d','e','f','g']
+print(l)
+print(l[1])
+print(l[1:3])
+print(l[1:-1])
+print(l[:3])
+print(l[2:])
+print(l[-4:-1])
+print(l[:-1])
+print(l[0:5:2])
+print(l[1::2])
+print(l[::-1])
+print(l[::-2])
+
