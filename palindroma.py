37b559712c7d4e44388bb2b8b1d35bfcd04b5055
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-08-20 11:03:55 +0530

    Add files via upload

---
+++ palindroma.py
@@ -0,0 +1,7 @@
+as_string = input("Enter a string\n")
+revs_string = as_string[::-1]
+if ( as_string == revs_string ):
+	print("The given string is a palindrome")
+else:
+	print("The given string is not a palindrome")
+	
