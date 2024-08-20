b055d15477462bf1cf25d149b25cbd632d449952
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-08-20 11:34:35 +0530

    Update evenlen.py

--- evenlen.py
+++ evenlen.py
@@ -1,6 +1,6 @@
 input_string = input("Enter a string\n")
 words = input_string.split()
-print("Even letter words are")
+print("Even letter words are:")
 for word in words:
 	if (len(word)%2 == 0):
 		print(word)
