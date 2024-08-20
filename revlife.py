d356f02d25b05b279b1cc0e3ec05c236f2f0160a
VarshithRaj007 <160224022+VarshithRaj007@users.noreply.github.com>
2024-08-20 10:51:20 +0530

    Update revlife.py

--- revlife.py
+++ revlife.py
@@ -2,5 +2,5 @@ input_string = input("Enter a string\n")
 rev_string = ""
 for char in input_string:
 	rev_string = char + rev_string
-print("Reversed String\n",rev_string)
+print("Reversed String\n"+rev_string)
 
